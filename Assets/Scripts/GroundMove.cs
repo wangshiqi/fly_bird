950a6efa8dad4fc833e9e1ad7098bc22220b0f3a
wangshiqi <wsq3082@yahoo.cn>
2014-03-03 20:20:31 +0800

    删除不用图片
    删除不用图片

--- Assets/Scripts/GroundMove.cs
+++ Assets/Scripts/GroundMove.cs
@@ -3,11 +3,6 @@ using System.Collections;
 
 public class GroundMove : MonoBehaviour {
 	public static float speed = 3.5f;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
 		if (BirdControll.gameOver) {
